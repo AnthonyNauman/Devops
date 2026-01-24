f78e259b9056b73d625e0aa547e1c1204e663422
nao <naumenko.ao@mail.ru>
2026-01-25 01:11:28 +0300

    default port

--- Run/run_client.bat
+++ Run/run_client.bat
@@ -2,4 +2,3 @@
 call "%~dp0\..\DevopsData\config.bat"
 
 start "%PROJECT_NAME%-%VERSION% Client" "%CLIENT_EXE_PATH%" -WINDOWED -ResX=600 -ResY=600 -WinX=10 -WinY=50 -ExecCmds="open 127.0.0.1:7777"
-start "%PROJECT_NAME%-%VERSION% Client" "%CLIENT_EXE_PATH%" -WINDOWED -ResX=600 -ResY=600 -WinX=620 -WinY=50 -ExecCmds="open 127.0.0.1:7777"
